5e93fcc326afa989b198374e08f8315f5119ecdf
Fabricio Floriano <f.florian0@hotmail.com>
2023-08-16 22:47:13 -0300

    Lint fix

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,7 +2,7 @@ const userRouter = require('express').Router();
 const userController = require('../controllers/userController');
 const validateDisplayName = require('../middlawares/validateDisplayName');
 const validateEmailAndPass = require('../middlawares/validateEmailAndPass');
-const validateToken = require('../middlawares/validateToken')
+const validateToken = require('../middlawares/validateToken');
 
 userRouter.post('/', validateDisplayName, validateEmailAndPass, userController.signUp);
 userRouter.get('/', validateToken, userController.getUsers);
