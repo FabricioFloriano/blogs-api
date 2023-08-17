7f31b04a6232152b961d925957979411aa9d6b0d
Fabricio Floriano <f.florian0@hotmail.com>
2023-08-17 10:46:44 -0300

    fix

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,6 +1,6 @@
 const categoryRoute = require('express').Router();
 const categoryController = require('../controllers/categoryController');
-const validateCategoryT = require('../middlawares/validadeCategoryT');
+const validateCategoryT = require('../middlawares/ValidadeCategoryT');
 const validateToken = require('../middlawares/validateToken');
 
 categoryRoute.post('/', validateCategoryT, categoryController.createCategory);
