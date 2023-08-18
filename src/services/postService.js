349f463e37d0db7c803df708a76c41229d7456f5
Fabricio Floriano <f.florian0@hotmail.com>
2023-08-18 03:47:47 -0300

    Requisito 14

--- src/services/postService.js
+++ src/services/postService.js
@@ -46,8 +46,29 @@ const getPosts = async () => {
     });
     return post;
 };
+const getPostsById = async (id) => {
+    const post = await BlogPost.findByPk(id, {
+        include: [
+            { 
+                model: User,
+                as: 'user',
+                attributes: { exclude: ['password'] },
+            },
+            { 
+                model: Category,
+                as: 'categories',
+                through: { attributes: [] },
+            },
+        ],
+    });
+
+    if (!post) return { message: 'Post does not exist' };
+
+    return post;
+};
 
 module.exports = {
     createPost,
     getPosts,
+    getPostsById,
 };
